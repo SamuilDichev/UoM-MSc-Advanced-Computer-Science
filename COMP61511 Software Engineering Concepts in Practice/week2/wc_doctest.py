b9c93b0b596037ed9587672d26fec3ca6fcb5678
SamuilDichev <samkata00@abv.bg>
2017-10-09 17:53:53 +0100

    Separated tests into stderr and stdout testing

--- COMP61511 Software Engineering Concepts in Practice/week2/wc_doctest.py
+++ COMP61511 Software Engineering Concepts in Practice/week2/wc_doctest.py
@@ -1,19 +1,18 @@
-def testAllInputs():
+def testAllInputsAndFlags():
   """
   >>> import subprocess
   >>> from itertools import permutations
-  >>> input = ["-", "-", "-", "w", "w", "c", "l"]
+  >>> perms_input = ["-", "w", "w", "c", "l"]
   >>> output = ""
-  >>> output = [c for i in range(1, 4) for c in permutations(input,i)]
+  >>> output = [c for i in range(1, 4) for c in permutations(perms_input,i)]
   >>> output = ["".join(a) for a in output]
-
   >>> passed, failed = 0, 0
-  >>> for combo in output:
+  >>> for flags in output[1:]:
   ...   try:
-  ...     wcOut = subprocess.check_output("wc testinputs/* 2>&1", shell=True)
+  ...     wcOut = subprocess.check_output("wc {} testinputs/* 2>/dev/null".format(flags), shell=True)
   ...   except subprocess.CalledProcessError as exc:
   ...     wcOut = exc.output
-  ...   wcPyOut = subprocess.check_output("python3 wc.py testinputs/* 2>&1", shell=True)
+  ...   wcPyOut = subprocess.check_output("python3 wc.py {} testinputs/* 2>/dev/null".format(flags), shell=True)
   ...   wcOut = [str(s) for s in wcOut.split()]
   ...   wcPyOut = [str(s) for s in wcPyOut.split()]
   ...   if wcOut == wcPyOut:
@@ -21,7 +20,20 @@ def testAllInputs():
   ...   else:
   ...     failed += 1
   >>> print(passed, failed)
-  259 0
+  84 0
+  >>> passed, failed = 0, 0
+  >>> for flags in output[1:]:
+  ...   try:
+  ...     wcOut = subprocess.check_output("wc {} testinputs/* 2>&1 >/dev/null".format(flags), shell=True)
+  ...   except subprocess.CalledProcessError as exc:
+  ...     wcOut = exc.output
+  ...   wcPyOut = subprocess.check_output("python3 wc.py {} testinputs/* 2>&1 >/dev/null".format(flags), shell=True)
+  ...   if wcOut == wcPyOut:
+  ...     passed += 1
+  ...   else:
+  ...     failed += 1
+  >>> print(passed, failed)
+  84 0
   """
 
 if __name__ == "__main__":
