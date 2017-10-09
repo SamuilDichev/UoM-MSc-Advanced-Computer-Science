54fbda03590d26d3558e630d0368ad9b00352e40
SamuilDichev <samkata00@abv.bg>
2017-10-09 16:49:18 +0100

    Made test generate flag combinations and test on testinputes

--- COMP61511 Software Engineering Concepts in Practice/week2/wc_doctest.py
+++ COMP61511 Software Engineering Concepts in Practice/week2/wc_doctest.py
@@ -1,19 +1,28 @@
 def testAllInputs():
   """
   >>> import subprocess
-  >>> try:
-  ...   wcOut = subprocess.check_output("wc testinputs/* 2>&1", shell=True)
-  ... except subprocess.CalledProcessError as exc:
-  ...   wcOut = exc.output
-  >>>
-  >>> wcPyOut = subprocess.check_output("python3 wc.py testinputs/* 2>&1", shell=True)
-  >>> wcOut = [str(s) for s in wcOut.split()]
-  >>> wcPyOut = [str(s) for s in wcPyOut.split()]
-  >>> wcOut == wcPyOut
-  True
-  """
+  >>> from itertools import permutations
+  >>> input = ["-", "-", "-", "w", "w", "c", "l"]
+  >>> output = ""
+  >>> output = [c for i in range(1, 4) for c in permutations(input,i)]
+  >>> output = ["".join(a) for a in output]
 
-# TODO Make the above vary generate combinations of flags.
+  >>> passed, failed = 0, 0
+  >>> for combo in output:
+  ...   try:
+  ...     wcOut = subprocess.check_output("wc testinputs/* 2>&1", shell=True)
+  ...   except subprocess.CalledProcessError as exc:
+  ...     wcOut = exc.output
+  ...   wcPyOut = subprocess.check_output("python3 wc.py testinputs/* 2>&1", shell=True)
+  ...   wcOut = [str(s) for s in wcOut.split()]
+  ...   wcPyOut = [str(s) for s in wcPyOut.split()]
+  ...   if wcOut == wcPyOut:
+  ...     passed += 1
+  ...   else:
+  ...     failed += 1
+  >>> print(passed, failed)
+  259 0
+  """
 
 if __name__ == "__main__":
   import doctest
