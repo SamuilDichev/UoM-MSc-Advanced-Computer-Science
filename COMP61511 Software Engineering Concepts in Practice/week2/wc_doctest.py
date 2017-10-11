4c36a0737391241602bccf672cd76220bef279b5
SamuilDichev <samkata00@abv.bg>
2017-10-11 17:07:55 +0100

    Added test generation functions to wc_doctest

--- COMP61511 Software Engineering Concepts in Practice/week2/wc_doctest.py
+++ COMP61511 Software Engineering Concepts in Practice/week2/wc_doctest.py
@@ -1,41 +1,43 @@
-def testAllInputsAndFlags():
-  """
-  >>> import subprocess
-  >>> from itertools import permutations
-  >>> perms_input = ["-", "w", "w", "c", "l"]
-  >>> output = ""
-  >>> output = [c for i in range(1, 4) for c in permutations(perms_input,i)]
-  >>> output = ["".join(a) for a in output]
-  >>> passed, failed = 0, 0
-  >>> for flags in output[1:]:
-  ...   try:
-  ...     wcOut = subprocess.check_output("wc {} testinputs/* 2>/dev/null".format(flags), shell=True)
-  ...   except subprocess.CalledProcessError as exc:
-  ...     wcOut = exc.output
-  ...   wcPyOut = subprocess.check_output("python3 wc.py {} testinputs/* 2>/dev/null".format(flags), shell=True)
-  ...   wcOut = [str(s) for s in wcOut.split()]
-  ...   wcPyOut = [str(s) for s in wcPyOut.split()]
-  ...   if wcOut == wcPyOut:
-  ...     passed += 1
-  ...   else:
-  ...     failed += 1
-  >>> print(passed, failed)
-  84 0
-  >>> passed, failed = 0, 0
-  >>> for flags in output[1:]:
-  ...   try:
-  ...     wcOut = subprocess.check_output("wc {} testinputs/* 2>&1 >/dev/null".format(flags), shell=True)
-  ...   except subprocess.CalledProcessError as exc:
-  ...     wcOut = exc.output
-  ...   wcPyOut = subprocess.check_output("python3 wc.py {} testinputs/* 2>&1 >/dev/null".format(flags), shell=True)
-  ...   if wcOut == wcPyOut:
-  ...     passed += 1
-  ...   else:
-  ...     failed += 1
-  >>> print(passed, failed)
-  84 0
-  """
+def generateTest():
+  with open("tests.txt", "w") as f:
+    f.write(">>> import subprocess\n")
+    for flags in generateFlagPermutations()[1:]:
+        f.write(generateTestString(flags))
+
+def generateTestString(flags):
+  testStr = (">>> subprocess.check_output(\"python3 wc.py {} testinputs/* 2>/dev/null\", shell=True)\n"
+  "{}\n"
+  ">>> subprocess.check_output(\"python3 wc.py {} testinputs/* 2>&1 >/dev/null\", shell=True)\n"
+  "{}\n").format(flags, getExpectedStdout(flags), flags, getExpectedStderr(flags))
+  return testStr
+
+def getExpectedStdout(flags):
+  try:
+    wcOut = subprocess.check_output("wc {} testinputs/* 2>/dev/null".format(flags), shell=True)
+  except subprocess.CalledProcessError as e:
+    wcOut = e.output
+
+  wcOut = "\\t".join(str(wcOut).split())
+
+  return wcOut
+
+def getExpectedStderr(flags):
+  try:
+    wcOut = subprocess.check_output("wc {} testinputs/* 2>&1 >/dev/null".format(flags), shell=True)
+  except subprocess.CalledProcessError as e:
+    wcOut = e.output
+
+  return wcOut
+
+def generateFlagPermutations():
+  perms_input = ["-", "w", "w", "c", "l"]
+  output = [c for i in range(1, 4) for c in permutations(perms_input, i)]
+  output = ["".join(a) for a in output]
+  return output
 
 if __name__ == "__main__":
   import doctest
-  doctest.testmod()
+  import subprocess
+  from itertools import permutations
+  # generateTest()
+  doctest.testfile("tests.txt")
