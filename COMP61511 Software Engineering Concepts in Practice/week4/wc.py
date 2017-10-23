0b252803839556e34e7a639cbea1cc1174eb6d03
SamuilDichev <samkata00@abv.bg>
2017-10-23 19:32:13 +0100

    Add -m, -L, --help, --version

--- COMP61511 Software Engineering Concepts in Practice/week4/wc.py
+++ COMP61511 Software Engineering Concepts in Practice/week4/wc.py
@@ -7,13 +7,12 @@ DIR_ERR = "wc: {}: Is a directory"
 PERMISSION_ERROR = "wc: {}: Permission denied"
 LONG_OPT_ERR = "wc: unrecognised option '{}'\nTry 'wc --help' for more information."
 SHORT_OPT_ERR = "wc: invalid option -- '{}'\nTry 'wc --help' for more information."
-ALLOWED_OPTIONS = ["l", "w", "c"]
 
 # WHAT THE FUCK, wc?
 ESCAPED_SYMBOLS = [" ", "!", "~", "#", "$", "^", "&", "*", "(", ")", "=", "<", ">", "?", ";", ":", "[", "{", "]", "}", "|"]
 
 def processFile(filepath):
-  linecount, wordcount, bytecount = 0, 0, 0
+  linecount, wordcount, bytecount, maxlc = 0, 0, 0, 0
 
   with open(filepath, 'rb') as f:
     for line in f:
@@ -21,30 +20,36 @@ def processFile(filepath):
       wordcount += len(line.split())
       bytecount += len(line)
 
-  return linecount, wordcount, bytecount, filepath
+      if len(line) > maxlc:
+        maxlc = len(line)
+
+  return {"lc": linecount, "maxlc": maxlc, "wc": wordcount, "cc": bytecount, "bc": bytecount, "fp": filepath}
 
 def processFiles(filepaths, options):
-  totalLC, totalWC, totalBC = 0, 0, 0
+  totals = {"lc": 0, "maxlc": 0, "wc": 0, "cc": 0, "bc": 0, "fp": "total"}
 
   for filepath in filepaths:
     try:
-      result = processFile(filepath)
-      lc, wc, bc = result[0], result[1], result[2]
-      totalLC += lc
-      totalWC += wc
-      totalBC += bc
+      results = processFile(filepath)
+      totals["lc"] += results.get("lc")
+      totals["wc"] += results.get("wc")
+      totals["cc"] += results.get("cc")
+      totals["bc"] += results.get("bc")
 
-      printOutput(lc, wc, bc, filepath, options)
+      if results.get("maxlc") > totals.get("maxlc"):
+        totals["maxlc"] = results.get("maxlc")
+
+      printOutput(results, options)
     except FileNotFoundError:
       eprint(FNF_ERR.format(escapeIllegalSymbols(filepath)))
     except IsADirectoryError:
       eprint(DIR_ERR.format(escapeIllegalSymbols(filepath)))
-      printOutput(0, 0, 0, filepath, options)
+      printOutput({"lc": 0, "wc": 0, "cc": 0, "bc": 0, "fp": filepath}, options)
     except PermissionError:
       eprint(PERMISSION_ERROR.format(escapeIllegalSymbols(filepath)))
 
   if len(filepaths) > 1:
-    printOutput(totalLC, totalWC, totalBC, "total", options)
+    printOutput(totals, options)
 
 def escapeIllegalSymbols(string):
   for c in string:
@@ -56,8 +61,13 @@ def escapeIllegalSymbols(string):
 def createArgParser():
   parser = argparse.ArgumentParser(add_help=False)
 
-  for option in ALLOWED_OPTIONS:
-    parser.add_argument("-{}".format(option), "--{}".format(option), action="store_true")
+  parser.add_argument("-l", "--l", "--lines", action="store_true", dest="l")
+  parser.add_argument("-w", "--w", "--words", action="store_true", dest="w")
+  parser.add_argument("-c", "--c", "--bytes", action="store_true", dest="c")
+  parser.add_argument("-m", "--m", "--chars", action="store_true", dest="m")
+  parser.add_argument("-L", "--max-line-length", action="store_true", dest="L")
+  parser.add_argument("--help", action="store_true", dest="help")
+  parser.add_argument("--version", action="store_true", dest="version")
 
   parser.add_argument("FILE", nargs="*")
 
@@ -82,18 +92,22 @@ def printBadArgsError(args):
   else:
     eprint(SHORT_OPT_ERR.format(args[0][1:]))
 
-def printOutput(lc, wc, bc, last, options):
+def printOutput(results, options):
   output = ""
-  options = [o for o in options if o in ALLOWED_OPTIONS]
+  options = [o for o in options if o in ["l", "w", "m", "c", "L"]]
 
-  if ALLOWED_OPTIONS[0] in options or len(options) == 0:
-    output += "\t{}".format(lc)
-  if ALLOWED_OPTIONS[1] in options or len(options) == 0:
-    output += "\t{}".format(wc)
-  if ALLOWED_OPTIONS[2] in options or len(options) == 0:
-    output += "\t{}".format(bc)
+  if "l" in options or len(options) == 0:
+    output += "\t{}".format(results.get("lc"))
+  if "w" in options or len(options) == 0:
+    output += "\t{}".format(results.get("wc"))
+  if "m" in options:
+    output += "\t{}".format(results.get("cc"))
+  if "c" in options or len(options) == 0:
+    output += "\t{}".format(results.get("bc"))
+  if "L" in options:
+    output += "\t{}".format(results.get("maxlc"))
 
-  output += "\t{}".format(last)
+  output += "\t{}".format(results.get("fp"))
 
   print(output)
 
@@ -118,6 +132,41 @@ def preprocessArgs(args):
 def isFileArg(arg):
   return not str(arg).startswith("-") or str(arg) == "-" or str(arg) == "--"
 
+def printHelp():
+  print("""Usage: wc [OPTION]... [FILE]...
+  or:  wc [OPTION]... --files0-from=F
+Print newline, word, and byte counts for each FILE, and a total line if
+more than one FILE is specified.  A word is a non-zero-length sequence of
+characters delimited by white space.
+
+With no FILE, or when FILE is -, read standard input.
+
+The options below may be used to select which counts are printed, always in
+the following order: newline, word, character, byte, maximum line length.
+  -c, --bytes            print the byte counts
+  -m, --chars            print the character counts
+  -l, --lines            print the newline counts
+      --files0-from=F    read input from the files specified by
+                           NUL-terminated names in file F;
+                           If F is - then read names from standard input
+  -L, --max-line-length  print the maximum display width
+  -w, --words            print the word counts
+      --help     display this help and exit
+      --version  output version information and exit
+
+GNU coreutils online help: <http://www.gnu.org/software/coreutils/>
+Full documentation at: <http://www.gnu.org/software/coreutils/wc>
+or available locally via: info '(coreutils) wc invocation'""")
+
+def printVersion():
+  print("""wc (GNU coreutils) 8.25
+Copyright (C) 2016 Free Software Foundation, Inc.
+License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+
+Written by Paul Rubin and David MacKenzie.""")
+
 if __name__ == "__main__":
   parser = createArgParser()
   args = preprocessArgs(sys.argv[1:])
@@ -126,6 +175,14 @@ if __name__ == "__main__":
   if len(badArgs) > 0:
     printBadArgsError(badArgs)
     sys.exit(1)
+
+  for arg in sys.argv[1:]:
+    if arg == "--help":
+      printHelp()
+      sys.exit(0)
+    elif arg == "--version":
+      printVersion()
+      sys.exit(0)
 
   # TODO Which error? Invalid zero-length or no such file?
   if len(sys.argv) < 2:
