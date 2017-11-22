f23b9e33c6596fa4b173491950fd6f85cf52b0d7
SamuilDichev <samkata00@abv.bg>
2017-11-22 14:31:59 +0000

    Add lab2 src code

---
+++ COMP61021/Labs/lab2/lab2_Part1/lab_som.m
@@ -0,0 +1,32 @@
+function som = lab_som (trainingData, neuronCount, trainingSteps, startLearningRate, startRadius)
+% som = lab_som (trainingData, neuronCount, trainingSteps, startLearningRate, startRadius)
+% -- Purpose: Trains a 1D SOM i.e. A SOM where the neurons are arranged
+%             in a single line. 
+%             
+% -- <trainingData> data to train the SOM with
+% -- <som> returns the neuron weights after training
+% -- <neuronCount> number of neurons 
+% -- <trainingSteps> number of training steps 
+% -- <startLearningRate> initial learning rate
+% -- <startRadius> initial radius used to specify the initial neighbourhood size
+
+% TODO:
+% The student will need to complete this function so that it returns
+% a matrix 'som' containing the weights of the trained SOM.
+% The weight matrix should be arranged as follows, where
+% N is the number of features and M is the number of neurons:
+%
+% Neuron1_Weight1 Neuron1_Weight2 ... Neuron1_WeightN
+% Neuron2_Weight1 Neuron2_Weight2 ... Neuron2_WeightN
+% ...
+% NeuronM_Weight1 NeuronM_Weight2 ... NeuronM_WeightN
+%
+% It is important that this format is maintained as it is what
+% lab_vis(...) expects.
+%
+% Some points that you need to consider are:
+%   - How should you randomise the weight matrix at the start?
+%   - How do you decay both the learning rate and radius over time?
+%   - How does updating the weights of a neuron effect those nearby?
+%   - How do you calculate the distance of two neurons when they are
+%     arranged on a single line?
